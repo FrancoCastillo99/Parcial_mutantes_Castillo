c44ad3dc1261034a91557a0af6cfa1cb4362d0f3
FrancoCastillo99 <Francocast_99@hotmail.com>
2024-10-14 01:09:50 -0300

    Se agregaron mas test

--- src/test/java/com/example/mutantes_castillo/service/MutantDetectionServiceTest.java
+++ src/test/java/com/example/mutantes_castillo/service/MutantDetectionServiceTest.java
@@ -41,4 +41,105 @@ class MutantDetectionServiceTest {
         boolean result = mutantService.isMutant(humanDNA);
         assertFalse(result, "El ADN no debe ser detectado como mutante");
     }
+
+    @Test
+    void testIsMutantWithExample1() {
+        String[] dna = {
+                "AAAA",
+                "CCCC",
+                "TCAG",
+                "GGTC"
+        };
+
+        boolean result = mutantService.isMutant(dna);
+        assertTrue(result, "El ADN debe ser detectado como mutante");
+    }
+
+    @Test
+    void testIsMutantWithExample2() {
+        String[] dna = {
+                "TGAC",
+                "AGCC",
+                "TGAC",
+                "GGTC"
+        };
+
+        boolean result = mutantService.isMutant(dna);
+        assertTrue(result, "El ADN debe ser detectado como mutante");
+    }
+
+    @Test
+    void testIsMutantWithExample3() {
+        String[] dna = {
+                "TGAC",
+                "ATCC",
+                "TAAG",
+                "GGTC"
+        };
+
+        boolean result = mutantService.isMutant(dna);
+        assertFalse(result, "El ADN no debe ser detectado como mutante");
+    }
+
+    @Test
+    void testIsMutantWithExample4() {
+        String[] dna = {
+                "AAAT",
+                "AACC",
+                "AAAC",
+                "CGGG"
+        };
+
+        boolean result = mutantService.isMutant(dna);
+        assertFalse(result, "El ADN no debe ser detectado como mutante");
+    }
+
+    @Test
+    void testIsMutantWithExample5() {
+        String[] dna = {
+                "AAAA",
+                "AAAA",
+                "AAAA",
+                "AAAA"
+        };
+
+        boolean result = mutantService.isMutant(dna);
+        assertTrue(result, "El ADN debe ser detectado como mutante");
+    }
+
+    @Test
+    void testIsMutantWithExample6() {
+        String[] dna = {
+                "TCGGGTGAT",
+                "TGATCCTTT",
+                "TACGAGTGA",
+                "AAATGTACG",
+                "ACGAGTGCT",
+                "AGACACATG",
+                "GAATTCCAA",
+                "ACTACGACC",
+                "TGAGTATCC"
+        };
+
+        boolean result = mutantService.isMutant(dna);
+        assertTrue(result, "El ADN debe ser detectado como mutante");
+    }
+
+    @Test
+    void testIsMutantWithExample7() {
+        String[] dna = {
+                "TTTTTTTTT",
+                "TTTTTTTTT",
+                "TTTTTTTTT",
+                "TTTTTTTTT",
+                "CCGACCAGT",
+                "GGCACTCCA",
+                "AGGACACTA",
+                "CAAAGGCAT",
+                "GCAGTCCCC"
+        };
+
+        boolean result = mutantService.isMutant(dna);
+        assertTrue(result, "El ADN debe ser detectado como mutante");
+    }
 }
